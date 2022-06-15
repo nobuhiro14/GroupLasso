93d33ac4173efc7ca50c8185822fa15d38272a66
Nobu Desktop <n1770250@gmail.com>
2022-06-15 19:04:10 +0900

    add log

--- main_supervised.py
+++ main_supervised.py
@@ -22,6 +22,7 @@ class Options():
         parser.add_argument("--epoch",type = int, default = 30, help="epoch of supervised learning for base_model")
         parser.add_argument("--bt_size", type=int, default=64, help ="batch size of supervised learning for base_model")
         parser.add_argument("--lr", type=float, default=0.01, help =" learning rate of SGD (supervised)")
+        parser.add_argument("--split_ratio", type=float, default=0.8, help =" Split ratio for train valid datasets")
         parser.add_argument("--momentum", type=float, default=0.9, help="momentum for SGD (supervised)")
         parser.add_argument("--lb_group", type=float, default=9*10**-6, help="parameter of lasso")
         parser.add_argument("--lb_l1", type=float, default=5*10**-5, help="parameter of lasso")
@@ -32,7 +33,25 @@ class Options():
     def get_args(self):
         return self.parser.parse_args()
 
+def split_dataset(data_set, split_ratio, order=None):
+    n_examples = len(data_set)
+    split_at = int(n_examples * split_ratio)
+    if split_at < 0:
+        raise ValueError('split_at must be non-negative')
+    if split_at > n_examples:
+        raise ValueError('split_at exceeds the dataset size')
 
+    if order is not None:
+        subset1_indices = order[0:split_at]
+        subset2_indices = order[split_at:n_examples]
+    else:
+        subset1_indices = list(range(0,split_at))
+        subset2_indices = list(range(split_at,n_examples))
+
+    subset1 = Subset(data_set, subset1_indices)
+    subset2 = Subset(data_set, subset2_indices)
+
+    return subset1, subset2
 
 class GroupLasso():
     def __init__(self,model,lb):
@@ -157,6 +176,9 @@ def main():
         json.dump(args.__dict__, f, indent=4)
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+    
+
+
     ############# prepare dataset ##############################
     transform = transforms.Compose([
         #transforms.Resize(size=(224, 224)),
@@ -165,9 +187,17 @@ def main():
             (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
         )
     ])
-    train_dataset = datasets.CIFAR10(
+    dataset = datasets.CIFAR10(
     root= './data', train = True,
     download =True, transform = transform)
+
+    train_dataset, val_dataset = split_dataset(dataset,args.split_ratio)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset
+        , batch_size = args.bt_size 
+        , shuffle = True)
+
+
     train_loader = torch.utils.data.DataLoader(train_dataset
         , batch_size = args.bt_size 
         , shuffle = True)
@@ -187,12 +217,17 @@ def main():
 
     n_total_step = len(train_loader)
     average_time = 0
+    acc_tp5 = 0
+    train_log = []
+    val_log = []
+    tp5_log = []
     for epoch in range(args.epoch):
         running_loss = 0
         start_time = time.time()
-        ls_min = float('inf')
-        ls_max = 0
+        acc_tp5 = 0
+        
         acc_train  = 0
+        acc_val = 0 
         for i , (imgs, labels) in enumerate(train_loader):
             imgs = imgs.to(device)
             labels = labels.to(device)
@@ -202,6 +237,10 @@ def main():
             n_corrects = (labels_hat.argmax(axis=1)==labels).sum().item()
             acc_train += 100*(n_corrects/labels.size(0))
             loss_value = criterion(labels_hat,labels)
+            _,tp_5_pred = labels_hat.topk(5,1,True,True)
+            tp_5_pred = tp_5_pred.t()
+            tp5_correct = tp_5_pred.eq(labels.view(1,-1).expand_as(tp_5_pred))
+            acc_tp5 += (tp5_correct.float().sum().sum())*(100/args.bt_size)
 
             if args.lasso_flag == 1:
                 loss_value += lss.get_group_lasso(vgg16)
@@ -221,37 +260,72 @@ def main():
             running_loss += loss_value.item()
             if (i+1) % 250 == 0:
                 print(f"epoch {epoch+1}/{args.epoch}, step: {i+1}/{n_total_step}: loss = {loss_value:.5f},acc = {100*(n_corrects/labels.size(0)):.2f}%")
+                print(f"top 5 acc : {(tp5_correct.float().sum().sum())*(100/args.bt_size):.2f}")
                 print()
+        
+        ######################### end of epoch ################################
         end_time = time.time() 
         ver_time = end_time - start_time 
         print(f"times per epoch : {ver_time:.4f} (sec)")
+        print(f"Top 5 accuracy : {acc_tp5/n_total_step:.2f} %")
         average_time += ver_time 
 
         if epoch %2 == 1 :
             measure_L2_norm(vgg16,args.save_pth,epoch)
 
-        if epoch %1 == 0 :
-            with torch.no_grad():
-                number_corrects = 0
-                number_samples = 0
-                for i, (imgs,labels) in enumerate(test_loader):
-                    imgs = imgs.to(device)
-                    labels = labels.to(device)
+        
+        with torch.no_grad():
+            number_corrects = 0
+            number_samples = 0
+            for i, (imgs,labels) in enumerate(val_loader):
+                imgs = imgs.to(device)
+                labels = labels.to(device)
 
-                    labels_hat = vgg16(imgs)
-                    #labels_hat = model(imgs)
-                    acc_train += 100*(n_corrects/labels.size(0))
-                    loss_value = criterion(labels_hat,labels)
-                    number_corrects += (labels_hat.argmax(axis=1)==labels).sum().item()
-                    number_samples += labels.size(0)
+                labels_hat = vgg16(imgs)
+                #labels_hat = model(imgs)
+                acc_val += 100*(n_corrects/labels.size(0))
+                loss_value = criterion(labels_hat,labels)
+                number_corrects += (labels_hat.argmax(axis=1)==labels).sum().item()
+                number_samples += labels.size(0)
+            
+            print(f"Val accuracy {(number_corrects / number_samples)*100}%")
+        
+        train_log.append(acc_train/n_total_step)
+        tp5_log.append(acc_tp5/n_total_step)
+        val_log.append(acc_val/n_total_step)
+    
+    loginfo = {}
+    loginfo["train"] = train_log 
+    loginfo["tp5"] = tp5_log 
+    loginfo["val"] = val_log 
+
+    fname = f"{args.save_pth}/training_log.mat"
+    scipy.io.savemat(fname,loginfo)
+        
+        
+
                 
-                print(f"Overall accuracy {(number_corrects / number_samples)*100}%")
 
 
     average_time = average_time / (epoch +1)
     print("**************************************")
     print(f"Average training time : {average_time:.4f} (sec)")
     print("**************************************")
+    with torch.no_grad():
+        number_corrects = 0
+        number_samples = 0
+        for i, (imgs,labels) in enumerate(test_loader):
+            imgs = imgs.to(device)
+            labels = labels.to(device)
+
+            labels_hat = vgg16(imgs)
+            #labels_hat = model(imgs)
+            acc_train += 100*(n_corrects/labels.size(0))
+            loss_value = criterion(labels_hat,labels)
+            number_corrects += (labels_hat.argmax(axis=1)==labels).sum().item()
+            number_samples += labels.size(0)
+        
+        print(f"test accuracy {(number_corrects / number_samples)*100}%")
     model_save_path = f"{args.save_pth}/vgg16.model"
     torch.save(vgg16.state_dict(), model_save_path)
     
